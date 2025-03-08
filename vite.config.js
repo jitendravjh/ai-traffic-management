04ba2826dd55837436640b0b890059d00082894c
Jitendra Verma <jitendravjh@gmail.com>
2025-03-08 17:29:23 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-traffic-management/',  // Ensure correct path for GitHub Pages
-  plugins: [react()]
+  base: '/ai-traffic-management/', // GitHub Pages base path
+  plugins: [react()],
 });
