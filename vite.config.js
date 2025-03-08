d86ade901c72823f6ba7d516a9a941f76057412d
Jitendra Verma <jitendravjh@gmail.com>
2025-03-08 17:33:54 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-traffic-management/', // GitHub Pages base path
   plugins: [react()],
+  base: './',  // Important for GitHub Pages
 });
