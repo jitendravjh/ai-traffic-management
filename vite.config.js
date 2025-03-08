88055d8990afced6effa8380787b72a073bdf6f1
Jitendra Verma <jitendravjh@gmail.com>
2025-03-08 17:43:08 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-traffic-management/', // GitHub Pages base path
   plugins: [react()],
 });
